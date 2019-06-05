b837df68c9685c1795e1e6db0d45eff034eb328c
Shengye Wang <i@shengye.wang>
2019-06-05 16:18:15 -0700

    Some cmake files are not installed and bitbake complains about that.

--- yocto_compile/21-setup-realsense-v2.sh
+++ yocto_compile/21-setup-realsense-v2.sh
@@ -12,10 +12,13 @@ CURRENT_SCRIPT_PATH="$( cd -P "$( dirname "$CURRENT_SCRIPT" )" && pwd )"
 
 cd $CURRENT_SCRIPT_PATH/intel_aero
 
+rm -rf meta-intel-realsense-v2
 git clone https://github.com/IntelRealSense/meta-intel-realsense.git meta-intel-realsense-v2
-echo 'BBLAYERS += "/intel_aero/poky/../meta-intel-realsense-v2"' >> poky/build/conf/bblayers.conf
+grep "meta-intel-realsense-v2" poky/build/conf/bblayers.conf || echo 'BBLAYERS += "/intel_aero/poky/../meta-intel-realsense-v2"' >> poky/build/conf/bblayers.conf
 
 echo 'require include/intel-librealsense.inc' > poky/build/conf/auto.conf
 echo 'CORE_IMAGE_EXTRA_INSTALL += "librealsense2 librealsense2-tools"' >> poky/build/conf/auto.conf
 echo 'CORE_IMAGE_EXTRA_INSTALL += "librealsense2-debug-tools"' >> poky/build/conf/auto.conf
 echo 'CORE_IMAGE_EXTRA_INSTALL += "librealsense2-examples"' >> poky/build/conf/auto.conf
+
+echo 'FILES_${PN} += "/usr/lib/cmake/*"' >> meta-intel-realsense-v2/recipes-support/librealsense/librealsense2.inc
